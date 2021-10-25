d19e7f7eb268ad644a79be999baecd78eea918b1
Andrea perez <82877301+AndreaPerezCalvo@users.noreply.github.com>
2021-10-25 13:35:46 +0200

    Add files via upload

---
+++ Interfaces/interfacesFormales/InterfacesFormales_padre.py
@@ -0,0 +1,19 @@
+from abc import abstractmethod
+from abc import ABCMeta
+
+class Mando(metaclass=ABCMeta):
+    @abstractmethod
+    def siguiente_canal(self):
+        pass
+
+    @abstractmethod
+    def canal_anterior(self):
+        pass
+
+    @abstractmethod
+    def subir_volumen(self):
+        pass
+
+    @abstractmethod
+    def bajar_volumen(self):
+        pass
